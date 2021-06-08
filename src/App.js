940a433bbf97e219cf31228f4e99e201e890b8a6
altria <junchen4933@gmail.com>
2021-06-09 10:45:32 +1200

    Init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,101 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import classes from './App.module.css';
+import classicTee from './Data/classic-tee.jpg';
+import ProductData from './Data/ProductData';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [products] = useState([  //array of different sizes
+        {
+            size: 'S'
+        },
+        {
+            size: 'M'
+        },
+        {
+            size: 'L'
+        },
+    ])
+    const [isShown, setIsShown] = useState(false);
+
+    return (
+        <div>
+            <header>
+                <nav className={classes.Topbar}>
+                    <div>
+                        <span onMouseEnter={() => setIsShown(true)}
+                            onMouseLeave={() => setIsShown(false)}
+                            onClick={() => setIsShown(true)}
+                            className={classes.cartbtn}>
+                            My cart({products.length})</span>
+                    </div>
+                    {isShown && (
+                    <div className={classes.cart}>
+                        <table className={classes.table}>
+                            {products.map((products) => (
+                                <tr className={classes.record}>
+                                    <td className={classes.miniImg}>
+                                        <img src={classicTee} alt="Product" />
+                                    </td>
+                                    <br></br>
+                                    <td>
+                                        <span className={classes.miniDesc}>
+                                            {ProductData.title}
+                                        </span>
+                                        <br></br>
+                                        <span className={classes.miniDesc}>
+                                            1 X
+                                </span>
+                                        <span className={classes.miniPrice}>
+                                            {ProductData.Price}
+                                        </span>
+                                        <br></br>
+                                        <span className={classes.miniDesc}>
+                                            Size: {products.size}
+                                        </span>
+                                        <br></br>
+                                    </td>
+                                </tr>
+                            ))}
+                        </table>
+                    </div>
+                    )}
+                </nav>
+
+            </header>
+            {/* end of header */}
+
+
+            <div className={classes.MainContainer}>
+                <div className={classes.ProductPicture}>
+                    <img src={classicTee} alt="Product" />
+                </div>
+
+                <div className={classes.ProductDetails}>
+                    <h1 className={classes.ProductTitle}>{ProductData.title}</h1>
+                    <h2 className={classes.ProductPrice}>{ProductData.Price}</h2>
+                    <p className={classes.ProductDesc}>{ProductData.Description}</p>
+
+                    <div className={classes.Size}>
+                        <span className={classes.SizeDesc}>SIZE</span>
+                        <span className={classes.Star}>*</span>
+                        <div>
+                            <button className={classes.Buttons}>S</button>
+                            <button className={classes.Buttons}>M</button>
+                            <button className={classes.Buttons}>L</button>
+                            <br></br>
+                            <button className={classes.Add}>{ProductData.Add}</button>
+                        </div>
+                    </div>
+                </div>
+
+
+
+            </div>
+        </div >
+    );
 }
+
+
 
 export default App;
---
+++ src/Data/ProductData.js
@@ -0,0 +1,8 @@
+const ProductData = {
+    title: 'Classic  Tee',
+    Price: "$75.00",
+    Description: "Dolor sit amet, consectetur adipiscing elit. Haec ettu ita posuisti, et verba vestra sunt. Quod autemratione actum est, id officium appellamus dolor sitamet, consectetur adipiscing elit. Haec et tu itaposuisti, et verba vestra sunt. Quod autem rationeactum est, id officium appellamus",
+    Add: "Add to cart"
+}
+
+export default ProductData
